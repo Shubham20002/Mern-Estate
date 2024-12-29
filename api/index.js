a6d9c9a143b1d598bfb45ed56edf8a61c01b5935
shubham <shubham992284@gmail.com>
2024-12-29 14:25:04 +0530

    crete api listing route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import { configDotenv } from "dotenv";
 import cookieParser from 'cookie-parser';
 import userRouter from '../api/routes/user.route.js'
 import authRouter from '../api/routes/auth.route.js'
+import listingRouter from './routes/listing.route.js';
 
 configDotenv();
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -23,6 +24,7 @@ app.listen(3000,()=>{
 
 app.use('/api/user',userRouter)
 app.use('/api/auth',authRouter)
+app.use('/api/listing', listingRouter);
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
